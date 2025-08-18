663ae53abb1c579c8202ced434911ddfaaeb11fb
Le Dinh Phuc <phucld@ominext.com>
2025-08-18 17:01:25 +0700

    first commit

---
+++ architecture-pattern-swift/architecture_pattern_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  architecture_pattern_swiftApp.swift
+//  architecture-pattern-swift
+//
+//  Created by Lauv Edward on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct architecture_pattern_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
